2ee14f19216485e102e81ee9d963055fe1a1592d
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-03-12 18:16:00 +0530

    trying to organize streams and their lifecycle.

--- app/imports/ui/containers/Room.js
+++ app/imports/ui/containers/Room.js
@@ -5,11 +5,12 @@ import { connect } from 'react-redux';
 // import Erizo from '../../modules/Erizo';
 
 import uiConfig from '../components/room/constants/uiConfig';
-
+import status from '../components/room/constants/status';
 // room components
 import StreamsContainer from '../components/room/StreamsContainer/';
 // import Sidebar from '../components/room/Sidebar';
 import Spotlight from '../components/room/Spotlight';
+
 
 class Room extends Component {
 
@@ -20,6 +21,19 @@ class Room extends Component {
     this.roomToken = localStorage.getItem(`roomToken:${this.roomName}`);
     /* eslint-disable new-cap */
     this.room = Erizo.Room({ token: this.roomToken });
+    // should I use `new` keyword here? licode docs dont use them, And If I did
+    // I may run into some issues due to context of `this` etc.
+
+    // an erizo data stream to be used for all 'data' purposes
+    this.primaryDataStream = Erizo.Stream({
+      data: true,
+      attributes: {
+        userId: this.roomUserId,
+        name: 'primary_data_stream',
+      },
+    });
+    this.setPrimaryDataStreamListners();
+    this.primaryDataStream.init();
     /* eslint-enable new-cap */
 
     this.calculateUISize = this.calculateUISize.bind(this);
@@ -28,12 +42,15 @@ class Room extends Component {
 
     this.state = {
       roomInfo: props.roomInfo,
-      connected: false,
-      tryingToConnect: true,
+      connectionStatus: status.TRYING_TO_CONNECT,
+      primaryDataStreamStatus: status.TRYING_TO_CONNECT,
+
       uiSize: this.calculateUISize(),
       streamContainerSize: uiConfig.COMPACT,
       roomHeight: innerHeight,
       roomWidth: innerWidth,
+
+
       settings: {
         uiBreakRatio: uiConfig.defaultBreakRatio,
         uiBreakWidth: uiConfig.defaultBreakWidth,
@@ -72,9 +89,9 @@ class Room extends Component {
   }
 
   tryToReconnect() {
-    if (this.state.connected || this.state.tryingToConnect || this.unmountInProgress) return;
+    if (this.state.connectionStatus === status.TRYING_TO_CONNECT || this.unmountInProgress) return;
     console.log('trying to reconnect');
-    this.setState({ ...this.state, tryingToConnect: true });
+    this.setState({ ...this.state, connectionStatus: status.TRYING_TO_CONNECT });
     this.props.joinRoom()
       .then(({ roomToken }) => {
         this.roomToken = roomToken;
@@ -90,14 +107,66 @@ class Room extends Component {
 
   setRoomConnectionListeners(room = this.room) {
     room.addEventListener('room-connected', (roomEvent) => {
-      console.log(roomEvent);
-      this.setState({ ...this.state, connected: true, tryingToConnect: false });
+      console.info('room connected', roomEvent);
+      console.log(room);
+      this.setRoomStreamListners();
+      this.setState({ ...this.state, connectionStatus: status.CONNECTED });
+
+      room.publish(this.primaryDataStream);
     });
 
-    room.addEventListener('room-disconnected', () => {
-      this.setState({ ...this.state, connected: false });
+    room.addEventListener('room-error', (roomEvent) => {
+      console.error('room connection error', roomEvent);
+      this.setState({ ...this.state, connectionStatus: status.DISCONNECTED });
+    });
+
+    room.addEventListener('room-disconnected', (roomEvent) => {
+      console.info('room disconnected', roomEvent);
+      this.setState({ ...this.state, connectionStatus: status.DISCONNECTED });
       this.tryToReconnect();
     });
+  }
+
+  setRoomStreamListners(room = this.room) { // when streams are added_to/removed_from the room
+    room.addEventListener('stream-added', (streamEvent) => {
+      console.log(streamEvent);
+      if (streamEvent.stream.getID() in room.localStreams) { // published by us
+        if (streamEvent.stream.getAttributes().name === 'primary_data_stream') {
+          this.setState({ ...this.state, primaryDataStreamStatus: status.CONNECTED });
+        }
+        // do not subscribe local streams
+        console.info('local stream added');
+        return;
+      }
+      room.subscribe(streamEvent.stream);
+      console.info('stream added', streamEvent);
+    });
+
+    room.addEventListener('stream-failed', (streamEvent) => {
+      console.error('stream failed', streamEvent);
+    });
+
+    room.addEventListener('stream-removed', (streamEvent) => {
+      console.info('stream removed', streamEvent);
+    });
+  }
+
+  setPrimaryDataStreamListners() {
+    const stream = this.primaryDataStream;
+    stream.addEventListener('access-accepted', (streamEvent) => {
+      console.info('primaryDataStream access-accepted', streamEvent);
+    });
+    stream.addEventListener('access-denied', (streamEvent) => {
+      console.error('primaryDataStream access-denied', streamEvent);
+    });
+    stream.addEventListener('stream-failed', (streamEvent) => {
+      this.setState({ ...this.state, primaryDataStreamStatus: status.ERROR });
+      console.error('primaryDataStream stream-failed', streamEvent);
+    });
+  }
+
+  setStreamListners(stream, options = null) {
+
   }
 
   onWindowResize(event) {
