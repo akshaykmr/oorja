e58caf7b5c37a90e60600ad57a5a8f43942efe0e
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-03-19 02:17:18 +0530

    fixes lint error.
    linting is a certified practice for avoiding health related issues in
    the future.

--- app/imports/ui/components/room/Spotlight/tabs/Info.js
+++ app/imports/ui/components/room/Spotlight/tabs/Info.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
-import Y from '../../../../../modules/Yjs';
+// import Y from '../../../../../modules/Yjs';
 
 class Info extends Component {
 
   componentDidMount() {
-    const { connectTab, dispatchMessage } = this.props; 
-    const y = new Y({
-      db: {
-        name: 'memory', // use memory database adapter.
-        // name: 'indexeddb' // use indexeddb database adapter instead for offline apps
-      },
-      connector: {
-        name: 'licodeConnector', // use webrtc connector
-        room: this.props.roomInfo.roomName, // clients connecting to the same room share data
-        role: 'slave',
-        registerMessageHandler,
-        dispatchMessage,
-      },
-      share: {
-        textarea: 'Text', // y.share.textarea is of type y-text
-      },
-    }).then((yinst) => {
-      console.log(yinst);
-      yinst.share.textarea.bind(document.querySelector('textarea'));
-    });
+    // const { connectTab, dispatchMessage } = this.props;
+    // const y = new Y({
+    //   db: {
+    //     name: 'memory', // use memory database adapter.
+    //     // name: 'indexeddb' // use indexeddb database adapter instead for offline apps
+    //   },
+    //   connector: {
+    //     name: 'licodeConnector', // use webrtc connector
+    //     room: this.props.roomInfo.roomName, // clients connecting to the same room share data
+    //     role: 'slave',
+    //     //registerMessageHandler,
+    //     dispatchMessage,
+    //   },
+    //   share: {
+    //     textarea: 'Text', // y.share.textarea is of type y-text
+    //   },
+    // }).then((yinst) => {
+    //   console.log(yinst);
+    //   yinst.share.textarea.bind(document.querySelector('textarea'));
+    // });
   }
   render() {
     return (
