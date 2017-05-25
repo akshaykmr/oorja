3fd7ec01fb045587b798e738ac895b8de64c866b
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-05-26 03:28:20 +0530

    redirect to https (for dev deployment) should be done by webserver ideally

--- app/imports/startup/client/index.js
+++ app/imports/startup/client/index.js
@@ -5,3 +5,9 @@ import '../../collections/client';
 import '../../collections/common';
 
 import './attemptReconnection';
+
+// this should be done by the webserver, here for dev deployment
+if (window.location.protocol !== 'https:') {
+  window.location = `https://${window.location.hostname}${window.location.pathname}
+  ${window.location.hash}`;
+}
