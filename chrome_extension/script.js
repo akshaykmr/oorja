49467cd45181406d94c9c1e695dfab702c77c5a0
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-06-05 19:53:53 +0530

    added chrome extension.

---
+++ chrome_extension/script.js
@@ -0,0 +1,25 @@
+
+// Listens for external messages coming from pages that match url pattern defined in manifest.json
+var bkg = chrome.extension.getBackgroundPage();
+chrome.runtime.onMessageExternal.addListener(
+    function(request, sender, sendResponse) {
+        bkg.console.log("Got request", request, sender);
+        if(request.getVersion) {
+            sendResponse({ version: chrome.runtime.getManifest().version});
+            return false; // Dispose of sendResponse
+        } else if(request.getStream) {
+            // Gets chrome media stream token and returns it in the response.
+            chrome.desktopCapture.chooseDesktopMedia(
+                ["screen", "window"], sender.tab,
+                function(streamId) {
+                    sendResponse({ streamId: streamId});
+                });
+            return true; // Preserve sendResponse for future use
+        } else {
+            console.error("Unknown request");
+            sendResponse({ error : "Unknown request" });
+            return false;
+        }
+    }
+);
+
