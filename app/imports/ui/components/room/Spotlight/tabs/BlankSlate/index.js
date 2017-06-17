53043fc176eb01223cfda952670007e4cf3f9381
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-06-18 02:15:24 +0530

    added link to chrome extension when initiliazation fails

--- app/imports/ui/components/room/Spotlight/tabs/BlankSlate/index.js
+++ app/imports/ui/components/room/Spotlight/tabs/BlankSlate/index.js
@@ -10,12 +10,20 @@ class BlankSlate extends Component {
   render() {
     return (
       <div className={this.props.classNames} style={this.props.style}>
-        <p>
+        <div className="joinUs">
         Are you a developer? You can extend functionality of oorja by developing a
         custom tab. oorja is open source and could use your skills !
-        <br/>
-        <a href="https://github.com/akshayKMR/oorja" target="_blank">Join us at GitHub</a>
-        </p>
+        <br/> <br/>
+        <a href="https://github.com/akshayKMR/oorja" target="_blank">Join us at GitHub 😎</a>
+        <br/><br/>
+        The tabs are react components using a simple but powerful mini-api
+        (using props and some event listeners) to add more capabilities to the room.
+        This is the coolest feature of this project.
+        It aids in easily configurable rooms during creation with features relevant to your
+        purpose, while also being able to add more later
+        It's exciting to think of the tabs people will develop leveraging the
+        super easy p2p interace/api in oorja.
+        </div>
       </div>
     );
   }
