24111e83fdcd33c85b2b787b0a3a3f6ebc102ef6
Akshay Kumar <akshay.kmr4321@gmail.com>
2018-03-29 01:56:11 +0530

    added Icons

---
+++ app/imports/ui/components/Icons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Loading = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-loader"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>
+
+export const PlusCircle = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>;
+
+export const Check = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+
+export const AlertCircle = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-alert-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>;
+
+export default {};
--- app/imports/ui/components/room/Spotlight/tabRegistry.js
+++ app/imports/ui/components/room/Spotlight/tabRegistry.js
@@ -1,6 +1,5 @@
 // need to fix linting in this file. (support dynamic import)
-
-
+import React from 'react';
 
 export default {
   1: {
@@ -10,7 +9,7 @@ export default {
     name: 'Info',
     displayName: 'Room Information',
 
-    icon: 'android-share-alt',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>,
     description: 'Invite others to this room',
 
     // Baground color for the div containing your tab content
@@ -36,7 +35,7 @@ export default {
     name: 'VideoChat',
     displayName: 'Video chat',
     bgColor: '#25272a',
-    icon: 'videocamera',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-video"><polygon points="23 7 16 12 23 17 23 7"></polygon><rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect></svg>,
     description: 'Video chat',
     streamContainerSize: '',
     load: () => import('./tabs/VideoChat'),
@@ -57,7 +56,7 @@ export default {
     name: 'QuillPad',
     displayName: 'QuillPad',
     bgColor: '#ffffff',
-    icon: 'document-text',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>,
     description: 'Shared Richtext document',
     streamContainerSize: '',
     load: () => import('./tabs/QuillPad')
@@ -67,7 +66,7 @@ export default {
     name: 'CodePad',
     displayName: 'CodePad',
     bgColor: '',
-    icon: 'code-working',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-code"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>,
     description: 'Shared Code editor',
     streamContainerSize: '',
     load: () => import('./tabs/CodePad')
@@ -77,7 +76,7 @@ export default {
     name: 'Chat',
     displayName: 'Chat',
     bgColor: '#faebd7',
-    icon: 'chatbubbles',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>,
     description: 'Chat messaging',
     streamContainerSize: '',
     load: () => import('./tabs/Chat')
@@ -87,7 +86,7 @@ export default {
     name: 'DiscoverTabs',
     displayName: 'Discover Tabs',
     bgColor: '#ffffff',
-    icon: 'ios-plus',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>,
     description: 'Discover more tabs',
     streamContainerSize: 'COMPACT',
     load: () => import('./tabs/DiscoverTabs')
@@ -97,7 +96,7 @@ export default {
     name: 'Reacteroids',
     displayName: 'Reacteroids',
     bgColor: '#ffffff',
-    icon: 'planet',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-target"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="6"></circle><circle cx="12" cy="12" r="2"></circle></svg>,
     description: 'Play a game while you wait',
     streamContainerSize: 'COMPACT',
     local: true,
@@ -108,7 +107,7 @@ export default {
     name: 'BlankSlate',
     displayName: 'Blank Slate',
     bgColor: '#ffffff',
-    icon: 'lightbulb',
+    icon: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-git-pull-request"><circle cx="18" cy="18" r="3"></circle><circle cx="6" cy="6" r="3"></circle><path d="M13 6h3a2 2 0 0 1 2 2v7"></path><line x1="6" y1="9" x2="6" y2="21"></line></svg>,
     description: 'For developers: A blank slate tab to be used as boilerplate for developing new tabs',
     streamContainerSize: 'COMPACT',
     local: false,
